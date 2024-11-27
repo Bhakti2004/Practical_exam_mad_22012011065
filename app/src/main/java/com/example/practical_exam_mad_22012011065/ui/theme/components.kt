8edf911dd5e4d5bd8072012df859c468f53352e2
Bhakti Kansagara <bhaktikansagara2004@gmail.com>
2024-11-27 09:02:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/practical_exam_mad_22012011065/ui/theme/components.kt
@@ -0,0 +1,2 @@
+package com.example.practical_exam_mad_22012011065.ui.theme
+
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Practical_exam_mad_22012011065"
+include(":app")
+ 
